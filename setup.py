98e7f6c3c3b22f5ef18a595a25e1f76d101c334a
Christian Göhring <cg@2601.me>
2023-06-21 23:22:50 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from sqldump import __version__
+
+setup(
+    name='sqldump',
+    version=__version__,
+
+    url='https://github.com/mostsignificant/sqldump',
+    author='Christian Göhring',
+    author_email='mostsig@gmail.com',
+
+    py_modules=['sqldump'],
+)
